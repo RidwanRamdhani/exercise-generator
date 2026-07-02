ac504e071c6af88e998a9ba0f5d22ede58d5b27a
RidwanRamdhani <gtiridwan@gmail.com>
2026-07-02 11:14:41 +0700

    feat: add seed exercises v2 dataset (Conditionals, Loops)

--- exercise-generator/test.py
+++ exercise-generator/test.py
@@ -1,10 +1,10 @@
-def binary_io(filename):
-    data_byte = b'\x48\x65\x6c\x6c\x6f'
-    with open(filename, 'wb') as f:
-        f.write(data_byte)
-    with open(filename, 'rb') as f:
-        return f.read().decode('utf-8')
+def sapa_tamu():
+    tamu = ['Andi', 'Budi', 'Citra']
+    hasil = []
+    for nama in tamu:
+        hasil.append(f'Halo {nama}, selamat datang!')
+    return hasil
     
-assert binary_io('data.bin') == 'Hello'
-assert binary_io('temp.bin') == 'Hello'
-assert isinstance(binary_io('sample.bin'), str)
+assert sapa_tamu() == ['Halo Andi, selamat datang!', 'Halo Budi, selamat datang!', 'Halo Citra, selamat datang!']
+assert len(sapa_tamu()) == 3
+assert all('Halo' in x for x in sapa_tamu())
